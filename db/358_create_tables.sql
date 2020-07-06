15f2b6d433e823ee780efcd8164a1a811bcbfe3b
PhilCAEUser <mail@rwth>
2020-07-06 08:35:50 +0000

    Edited db schema

--- db/358_create_tables.sql
+++ db/358_create_tables.sql
@@ -1,12 +1,12 @@
 --
--- Database Schema:  commedit
+-- Database Schema:  caeschema
 -- Automatically generated sql script for the service 358, created by the CAE.
 -- --------------------------------------------------------
 
 --
 -- Table structure for table todo_items.
 --
-CREATE TABLE commedit.todo_items (
+CREATE TABLE caeschema.todo_items (
   id INTEGER ,
   item_name VARCHAR(255) ,
 CONSTRAINT id_PK PRIMARY KEY (id)
