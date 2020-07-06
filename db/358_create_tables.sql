7ac3b9d9b0d9a886cda8927682f012ff7ca923ab
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-06 08:25:35 +0000

    Added GET method for TODO items

---
+++ db/358_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 358, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table todo_items.
+--
+CREATE TABLE commedit.todo_items (
+  id INTEGER ,
+  item_name VARCHAR(255) ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
